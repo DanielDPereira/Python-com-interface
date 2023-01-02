5ccb4c790f31532d409d6e9c854415101e39aec5
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-01-01 21:17:32 -0300

    Update Bhaskara.py

--- Bhaskara.py
+++ Bhaskara.py
@@ -7,11 +7,7 @@ layout = [  [sg.Text('Number 1'), sg.InputText()],
             [sg.Text('Number 3'), sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
-#Bhaskara 
 
-a = int(values[0])
-b = int(values[1])
-c = int(values[2])
 
 # Create the Window
 window = sg.Window('Window Title', layout)
@@ -20,6 +16,13 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    
+    #Bhaskara
+    
+    a = int(values[0])
+    b = int(values[1])
+    c = int(values[2])
+  
+    print('You entered ', a, b, c,)
 
 window.close()
