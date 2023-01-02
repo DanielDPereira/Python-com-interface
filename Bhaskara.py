c41d6656a2ab86b89d76b5a02ca08cb3fc7b4094
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-01-02 09:28:54 -0300

    Update Bhaskara.py

--- Bhaskara.py
+++ Bhaskara.py
@@ -23,6 +23,11 @@ while True:
     b = float(values[1])
     c = float(values[2])
     
+    delta = b**2 - 4*a*c
+    
+    if delta > 0:
+        
+    
     #-b+-(delta)**0.5/2*a
   
     print('You entered ', a, b, c,)
